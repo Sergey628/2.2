76950390dd5bec70b1fcb94e3e4ff53e1ffd518c
Sergey Lipatov <sergey0737@gmail.com>
2022-06-22 00:18:41 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Расчет бонусов за связь");
-        int score = 100;  // Начальный счет
-        int deposit = 55;      // Сумма пополнения
+        int initialAccount = 100;  // Начальный счет
+        int deposit = 8855;      // Сумма пополнения
         System.out.println(" Сумма пополнения " + deposit);
-        int x;            // Счет всего, без бонуса
-        x = score + deposit;
-        int bonus = deposit / 100;  // Бонус
-        int z = x + bonus;  // Счет всего с бонусом
+        int account;            // Счет всего, без бонуса
+        account = initialAccount + deposit;
+        int threshold = deposit / 100;  // Порог бонуса
+        int totalAmount = account + threshold;  // Счет всего с бонусом
 
         if (deposit >= 1000) {
-            System.out.println(" Итоговый счет " + z + " Бонусов " + bonus);
+            System.out.println(" Итоговый счет " + totalAmount + " Бонусов " + threshold);
         } else {
-            System.out.println(" Итоговый счет " + x + " Бонусов 0 ");
+            System.out.println(" Итоговый счет " + account + " Бонусов 0 ");
         }
-        ;
         System.out.println("Ура!!! Заработала!:-)");
 
 
